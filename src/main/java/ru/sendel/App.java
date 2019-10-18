fa7bdabf90434e30ed1cb5a0884c683dd0374600
shibkov.k <shibkov.k@gmail.com>
2019-10-18 13:50:50 +0500

    dependency to tomcat package

--- src/main/java/ru/sendel/App.java
+++ src/main/java/ru/sendel/App.java
@@ -3,9 +3,10 @@ package ru.sendel;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class App {
+public class App extends SpringBootServletInitializer{
    public static void main(String[] args) {
       SpringApplication.run(App.class, args);
    }
